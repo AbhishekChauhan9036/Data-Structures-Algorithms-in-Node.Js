d418b4203885119a9e85af9f2c5eb39231ab6e01
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-01-08 21:23:33 +0530

    Done

--- Practice/fibonacci.js
+++ Practice/fibonacci.js
@@ -7,16 +7,24 @@ function fibonacci(n){
     return fib;
 }
 
-const numFib=(n)=>{
+function numFib(n){
     let n1=0;
     let n2=1;let sum=0;
     for(let i=1;i<=n;i++){
+        if(i==1){
+            console.log(n1);
+            continue;
+        }
+        if(i==2) {
+            console.log(n2);
+            continue;
+        }
         sum=n1+n2;
         n1=n2;
         n2=sum;
         console.log(sum);
     }
-};
+}
 
 console.log(fibonacci(4));
 numFib(3);
