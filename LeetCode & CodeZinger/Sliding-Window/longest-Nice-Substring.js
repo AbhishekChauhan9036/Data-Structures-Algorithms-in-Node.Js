ecfb858b3d8f9d5f841ab5e1aa852f03b8486598
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-03-02 09:00:57 +0530

    Sliding Window

---
+++ LeetCode & CodeZinger/Sliding-Window/longest-Nice-Substring.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function longestNiceSubstring(s) {
+    if (s.length <=1) return "";
+    const set = new Set();
+    for (const c of s) set.add(c);
+
+    for (let i = 0; i <s.length; i++) {
+        const upperCaseChar = s[i].toUpperCase();
+        const lowerCaseChar = s[i].toLowerCase();
+
+        if (set.has(upperCaseChar) && set.has(lowerCaseChar)) {
+            continue; 
+        }
+        var str1=longestNiceSubstring(s.substring(0,i));
+        var str2=longestNiceSubstring(s.substring(i+1));
+        return str1.length>=str2.length?str1:str2;
+    }
+    return s;
+};
