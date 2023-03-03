01294bf6a3d0c99898304326280ff0ef6f5e3a7f
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-03-03 06:59:42 +0530

    Frequency-Counter-Method

---
+++ LeetCode & CodeZinger/Frequency-Method/frequency-count-method.js
@@ -0,0 +1,15 @@
+https://docs.google.com/document/d/1fkjb8RQ-WRcHsP6jIgPK1Q1WxhCNEMkPSKQ5XBKIJOk/edit
+
+
+Frequency Count Method:-
+
+What is the “Frequency Counter” pattern?
+When should we use the Frequency Counter pattern?
+
+Practice questions on Frequency Counter
+
+1.https://leetcode.com/problems/valid-anagram/
+2.https://leetcode.com/problems/top-k-frequent-elements/
+3.https://leetcode.com/problems/sort-characters-by-frequency/
+4.https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+5.Word Frequency Count Problem
