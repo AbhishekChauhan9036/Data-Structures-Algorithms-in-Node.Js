622e9281a545ac2c1d7dd6bf789395e00ae127a2
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-03-10 21:47:06 +0530

    count good number

--- LeetCode & GeeksForGeeks/Recursion/count-good-numbers.js
+++ LeetCode & GeeksForGeeks/Recursion/count-good-numbers.js
@@ -1,5 +1,4 @@
 var myPow = function(x,y){
-    var mod = 1000000007n;
     if(y===0)return 1n;
     var temp = 1n;
     temp *= myPow(x,parseInt(y/2)); 
@@ -7,14 +6,13 @@ var myPow = function(x,y){
     if(y%2===1){
         temp *= BigInt(x);
     }
-    temp %= mod;
+    temp %= 1000000007n;
     return temp;    
 }
 var countGoodNumbers = function(n) {
-    var mod = 1000000007n;
     var even = Math.ceil(n/2);
     var odd = n - even; 
     var ans = myPow(5, even) * myPow(4, odd);
-    ans %= mod; 
+    ans %= 1000000007n; 
     return Number(ans);
 };
