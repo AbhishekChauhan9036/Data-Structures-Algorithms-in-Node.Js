659cf0af06f430c842fe7cb6f23a888149b6f09f
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-03-15 15:56:39 +0530

    Searching & Sorting

---
+++ LeetCode & GeeksForGeeks/Searching-Sorting/sorting-and-searching.js
@@ -0,0 +1,62 @@
+https://docs.google.com/document/d/1Bxcy1GB57iczwFYnuI3enpDv_Hhgd93xdWSP8lYLglk/edit
+
+
+Searching:-
+Q1. What do you understand by a searching algorithm? List a few types of searching algorithms with an example.
+Q2.Describe the following along with their complexity :
+a).Linear Search            b). Binary Search        
+Q3. Differentiate between 
+ a). Linear & Binary Search               b). Sorting & Searching
+
+Practice question:-
+ 01.https://leetcode.com/problems/search-insert-position/
+02.https://leetcode.com/problems/search-a-2d-matrix/
+03. https://leetcode.com/problems/binary-search/
+04.https://leetcode.com/problems/sqrtx/
+05.https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+06.More Practice Problem
+https://docs.google.com/document/d/1DPmm6dJNoJD_RfBHdcI3KsPxfzujr6koGABmL_zArhQ/edit
+Solve these Problems on VsCode:-
+
+Q1. Find the largest three distinct elements in an array
+
+Input: arr[] = {10, 4, 3, 50, 23, 90}
+Output: 90, 50, 23
+
+Q2.Find the Missing Number
+Given an array arr[] of size N-1 with integers in the range of [1, N], the task is to find the missing number from the first N integers.
+Note: There are no duplicates in the list.
+Input: arr[] = {1, 2, 4, 6, 3, 7, 8}, N = 8
+Output: 5
+
+Q3.Take an integers of array by yourself and Perform following operations Search, insert and delete in a sorted array
+
+Q4.Binary Search for Rational Numbers without using floating point arithmetic
+A rational is represented as p/qb, for example 2/3. Given a sorted array of rational numbers, how to search an element using Binary Search. Use of floating-point arithmetic is not allowed.
+Input:  arr[] = {1/5, 2/3, 3/2, 13/2}
+x = 3/2
+Output: Found at index 2
+
+
+
+
+
+
+
+
+
+
+
+Sorting:-
+Q1.How many types of Sorting? Name them
+Q2.Explain these terms with its time Complexity in worst case , Average Case & Best case:- 
+1. Insertion sort   2. Selection sort  3.Bubble sort   4. Quick sort   5. Merge sort
+Q3.Differentiate  between:-
+Insertion sort  Vs  Selection sort  Vs  Bubble sort  Vs  Quicksort Vs Merge sort
+Practice questions on Sorting:-
+01. https://leetcode.com/problems/merge-intervals
+02. https://leetcode.com/problems/insert-interval
+03. https://leetcode.com/problems/maximum-gap
+04. https://leetcode.com/problems/find-k-pairs-with-smallest-sums
+05. https://leetcode.com/problems/k-th-smallest-prime-fraction
+
