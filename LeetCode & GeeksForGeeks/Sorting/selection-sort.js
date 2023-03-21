920ba9e4c8bfba1a8ffcf8cb5394fecbbf474771
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-03-21 19:27:20 +0530

    LC & GFG

---
+++ LeetCode & GeeksForGeeks/Sorting/selection-sort.js
@@ -0,0 +1,16 @@
+function selectionSort(arr, n) {
+    var i, j, min_idx;
+    for (i = 0; i < n - 1; i++) {
+        idx = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[idx])
+                idx = j;
+
+        [[arr[i], arr[idx]] = [arr[idx], arr[i]]];
+    }
+    return arr;
+}
+
+var arr = [64, -25, -120, 202, 11];
+var n = arr.length;
+console.log(selectionSort(arr, n));
